b395f3adf9f31a873e2950e55eb5c973d07df145
Ubuntu <ubuntu@ip-10-0-0-252.us-west-2.compute.internal>
2016-02-05 15:44:00 +0000

    refactor

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-Helper for creating Swagger from FHIR APIs
-======
+# FHIR to Swagger
+Generate Swagger from a FHIR conformance profile
+
+## Usage
+### Command Line
+```bash
+npm install -g fhir-swagger
+```
+
+### NodeJS
+```bash
+npm install --save fhir-swagger
+```
--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-var FS = require('fs');
 var Request = require('request');
 var Converter = require('./lib/conformance-to-swagger.js');
 
-var args = require('yargs').argv
-
-var baseURL = args.fhir_url || 'http://argonaut.healthintersections.com.au/open';
-var confPath = args.conformance_path || '/metadata?_format=application/json';
-var outputFile = args.output || './swagger.json';
-
-Request(baseURL + confPath, {json: true}, function(err, resp, body) {
-  if (err) throw err;
-  var swagger = Converter.convert(baseURL, body);
-  FS.writeFileSync(outputFile, JSON.stringify(swagger, null, 2))
-})
+module.exports = function(baseURL, confPath, callback) {
+  callback = callback || function(err) {if (err) throw err};
+  Request(baseURL + confPath, {json: true}, function(err, resp, body) {
+    if (err) return callback(err);
+    var swagger = Converter.convert(baseURL, body);
+    callback(null, swagger);
+  })
+}
