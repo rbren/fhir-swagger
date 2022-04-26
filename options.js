1af25fd1846967cfd5fa4158d2cce7e22e80572b
Frank <blankespoor51@gmail.com>
2022-04-26 08:38:06 +0200

    patch for running from cli or npm script

--- options.js
+++ options.js
@@ -1,4 +1,10 @@
-
+var args = require('yargs').argv;
+args = module.exports = JSON.parse(JSON.stringify(args));
+args.fhir_url = args.fhir_url || 'http://argonaut.healthintersections.com.au/open';
+args.conformance_path = args.conformance_path || '/metadata?_format=application/json';
+args.output = args.output || '-';
+args.fhir_cs_path=(/[A-z]:\//.test(args.fhir_cs_path))?args.fhir_cs_path:process.cwd()+args.fhir_cs_path;
+args.schemaPath=(/[A-z]:\//.test(args.schemaPath))?args.schemaPath:process.cwd()+args.schemaPath;
 
 // console.log(args)
 // process.exit()
