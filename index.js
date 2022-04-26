81343ffc24f9ec586db929f04b154245b5c41a6d
Frank <blankespoor51@gmail.com>
2022-04-26 08:29:16 +0200

    change to hopefully enforce options in cli aswell

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ var Converter = require('./lib/conformance-to-swagger.js');
 var FS = require('fs');
 
 module.exports = function(options, callback) {
-    // console.log(options)
+    options=formatOptions(options);
     var body = FS.readFileSync(options.fhir_cs_path, "utf-8");
     var json = JSON.parse(body);
     var swagger = Converter.convert(options.fhir_url, json,options);
     return callback(null, swagger);
+}
+
+function formatOptions(args){
+    
+    args = module.exports = JSON.parse(JSON.stringify(args));
+    args.fhir_url = args.fhir_url || 'http://argonaut.healthintersections.com.au/open';
+    args.conformance_path = args.conformance_path || '/metadata?_format=application/json';
+    args.output = args.output || '-';
+    args.fhir_cs_path=(/[A-z]:\//.test(args.fhir_cs_path))?args.fhir_cs_path:process.cwd()+args.fhir_cs_path;
+    args.schemaPath=(/[A-z]:\//.test(args.schemaPath))?args.schemaPath:process.cwd()+args.schemaPath;
+    return args;
 }
--- options.js
+++ options.js
@@ -1,6 +1,4 @@
-var args = require('yargs').argv;
-args = module.exports = JSON.parse(JSON.stringify(args));
-args.fhir_url = args.fhir_url || 'http://argonaut.healthintersections.com.au/open';
-args.conformance_path = args.conformance_path || '/metadata?_format=application/json';
-args.output = args.output || '-';
 
+
+// console.log(args)
+// process.exit()
