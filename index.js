7fb18a5e7d776dd6788a5e5c0a84c83b688a724c
Frank <blankespoor51@gmail.com>
2022-06-03 13:54:07 +0200

    typo fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var FS = require('fs');
 module.exports = function(options, callback) {
     // console.log(options)
     var body = FS.readFileSync(options.fhir_cs_path, "utf-8");
-    if(options.fhir_cs_path.endswith('.xml')){
+    if(options.fhir_cs_path.endsWith('.xml')){
         xml2js.parseString(body,(err,json)=>{
             if(err){
                 throw err;
