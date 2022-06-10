2d2d62273d8ab5f0f5b15ec312631ffd586f5ea2
Frank <blankespoor51@gmail.com>
2022-06-10 09:01:36 +0200

    removed no longer used dependencies

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var Converter = require('./lib/conformance-to-swagger.js');
-const xml2js= require("xml2js")
+const { XMLParser, }= require("fast-xml-parser")
 var FS = require('fs');
 
 module.exports = function(options, callback) {
-    // console.log(options)
     var body = FS.readFileSync(options.fhir_cs_path, "utf-8");
     if(options.fhir_cs_path.endsWith('.xml')){
-        xml2js.parseString(body,(err,json)=>{
-            if(err){
-                throw err;
-            }
-            Converter.convert(options.fhir_url, json.CapabilityStatement,options).then(swagger=>{
-                callback(null,swagger);
-            });
+        let xmlOptions = {
+            ignoreAttributes: false,
+            ignoreDeclaration: true,
+            attributeNamePrefix: ""
+        };
+        const parser = new XMLParser(xmlOptions);
+        let jsonObj = parser.parse(body);
+        let json =recurse(jsonObj);
+        // console.log(jsonObj)
+        Converter.convert(options.fhir_url, json.CapabilityStatement,options).then(swagger=>{
+            callback(null,swagger);
         });
     }else{
         var json = JSON.parse(body);
@@ -20,6 +23,15 @@ module.exports = function(options, callback) {
             callback(null,swagger);
         });
     }
-   
-    // return callback(null, swagger);
 }
+function recurse(obj) {     
+    for ( var key in obj ) { // works for objects and arrays 
+        var item = obj[key];
+        if(item.value){
+            obj[key]=item.value
+        } 
+        else if ( typeof item === "object" ) 
+            obj[key]=recurse(item); 
+    } 
+    return obj;
+  } 
